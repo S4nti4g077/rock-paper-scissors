67d6965cf034b1f8232467d3fbc6c402e49f4c21
Your Name <santiago.perez777@gmail.com>
2022-08-31 09:16:32 +0200

    added reset button, cleaned code

--- source/app.js
+++ source/app.js
@@ -3,90 +3,117 @@ const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 const result = document.querySelector("#result");
-const form = document.querySelector("#select");
-let counterNum = 0;
+const reset = document.querySelector("#reset");
 
 const gameBoard = {
   player1: true,
   player2: false,
   firstChose: 0,
   secondChose: 0,
-  score: 0,
+  p1score: 0,
+  p2score: 0,
 };
 
-const rockPress = rock.addEventListener("click", function (e) {
+function turnInfo() {
+  if (gameBoard.player1) {
+    info.innerHTML = "Player 1 select:";
+    return;
+  } else {
+    info.innerHTML = "Player 2 select:";
+    return;
+  }
+}
+function p1Turn() {
+  gameBoard.player1 = false;
+  gameBoard.player2 = true;
+  turnInfo();
+}
+function p2Turn() {
+  gameBoard.player1 = true;
+  gameBoard.player2 = false;
+  turnInfo();
+}
+
+rock.addEventListener("click", function (e) {
   e.preventDefault();
   if (gameBoard.player1) {
-    gameBoard.player1 = false;
-    gameBoard.player2 = true;
+    p1Turn();
     gameBoard.firstChose = "rock";
     return;
   }
   if (gameBoard.player2) {
-    gameBoard.player1 = true;
-    gameBoard.player2 = false;
+    p2Turn();
     gameBoard.secondChose = "rock";
     return game(gameBoard.firstChose, gameBoard.secondChose);
   }
 });
 
-const paperPress = paper.addEventListener("click", function (e) {
+paper.addEventListener("click", function (e) {
   e.preventDefault();
   if (gameBoard.player1) {
-    gameBoard.player1 = false;
-    gameBoard.player2 = true;
+    p1Turn();
     gameBoard.firstChose = "paper";
     return;
   }
   if (gameBoard.player2) {
-    gameBoard.player1 = true;
-    gameBoard.player2 = false;
+    p2Turn();
     gameBoard.secondChose = "paper";
     return game(gameBoard.firstChose, gameBoard.secondChose);
   }
 });
 
-const scissorsPress = scissors.addEventListener("click", function (e) {
+scissors.addEventListener("click", function (e) {
   e.preventDefault();
   if (gameBoard.player1) {
-    gameBoard.player1 = false;
-    gameBoard.player2 = true;
+    p1Turn();
     gameBoard.firstChose = "scissors";
     return;
   }
   if (gameBoard.player2) {
-    gameBoard.player1 = true;
-    gameBoard.player2 = false;
+    p2Turn();
     gameBoard.secondChose = "scissors";
     return game(gameBoard.firstChose, gameBoard.secondChose);
   }
 });
 
-const player1 = document.querySelector("#select");
 function game(player1, player2) {
   if (player1 === "rock") {
     if (player2 === "rock") {
       console.log("draw");
     } else if (player2 === "paper") {
-      console.log("player 2 wins!");
+      gameBoard.p2score++;
+      console.log("player 2 wins!", gameBoard.p2score);
     } else if (player2 === "scissors") {
-      console.log("player 1 wins!");
+      gameBoard.p1score++;
+      console.log("player 1 wins!", gameBoard.p2score);
     }
   } else if (player1 === "paper") {
     if (player2 === "paper") {
       console.log("draw");
     } else if (player2 === "scissors") {
-      console.log("player 2 wins!");
+      gameBoard.p2score++;
+      console.log("player 2 wins!", gameBoard.p2score);
     } else if (player2 === "rock") {
-      console.log("player 1 wins!");
+      gameBoard.p1score++;
+      console.log("player 1 wins!", gameBoard.p1score);
     }
   } else if (player1 === "scissors") {
     if (player2 === "rock") {
-      console.log("player 2 wins!");
+      gameBoard.p2score++;
+      console.log("player 2 wins!", gameBoard.p2score);
     } else if (player2 === "paper") {
-      console.log("player 2 wins!");
+      gameBoard.p1score++;
+      console.log("player 1 wins!", gameBoard.p1score);
     } else if (player2 === "scissors") {
       console.log("draw");
     }
   }
+  result.innerHTML = `P1: ${gameBoard.p1score} : P2: ${gameBoard.p2score}`;
 }
+
+reset.addEventListener("click", function (e) {
+  e.preventDefault();
+  gameBoard.p1score = 0;
+  gameBoard.p2score = 0;
+  result.innerHTML = `P1: ${gameBoard.p1score} : P2: ${gameBoard.p2score}`;
+});
